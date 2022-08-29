5042e21b41507c0c77b7730bdf025f772d751f11
Kasra <kasra@kasras-mbp.lan>
2022-08-29 12:11:24 -0400

    first try

--- app.py
+++ app.py
@@ -2,3 +2,28 @@ import streamlit as st
 
 
 st.title("Hello World! 2.0")
+
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
+# Perform query.
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.experimental_memo to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
+
+rows = run_query("SELECT * FROM `itracker-360915.itracker.itracker` LIMIT 1000")
+
+# Print results.
+st.write("Some wise words from Shakespeare:")
+for row in rows:
+    st.write("✍️ " + row['date'])
